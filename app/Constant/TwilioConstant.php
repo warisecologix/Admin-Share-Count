7993fc26c4cdd0bc60dcc2dc19708af3f43c397c
mwariszargar <muhammad.waris@ecologix.us>
2021-06-16 12:33:37 +0500

    Add twillio constant

---
+++ app/Constant/TwilioConstant.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Constant;
+
+class TwilioConstant
+{
+    const TWILIO_SID = "ACe1a8d38c382c1d74697166d2d9708ae1";
+    const TWILIO_AUTH_TOKEN = "b1e6b427e81f31928f9ee58426445110";
+    const TWILIO_VERIFY_SID = "VA9ea2a5b0934ee1d29d2a2c11efc64d1e";
+    const TWILIO_NUMBER = "+15102964187";
+    const TWILIO_VERIFY_NUMBER = "+92 323 4812995";
+    const TWILIO_ACCOUNT_STATUS = false;
+}
